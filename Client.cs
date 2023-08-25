7a3645536090e60fa2527410cfe2c96cc364a9dc
hamza <hamzacoskun41@gmail.com>
2023-08-25 12:41:05 +0300

    refact: add configuration and seperate client operations

---
+++ Client.cs
@@ -0,0 +1,77 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDBBenchmark.Generator;
+
+namespace MongoDBBenchmark;
+
+public class Client
+{
+
+    private IMongoCollection<BsonDocument> _collection;
+
+    public void Connect(string connectionString, string databaseName, string collectionName)
+    {
+        MongoClient client;
+        try {
+            client = new MongoClient(connectionString);
+        } catch (Exception e) {
+            Console.WriteLine("Could not connected to the database.");
+            Console.WriteLine(e.Message);
+            return;
+        }
+        _collection =  client
+            .GetDatabase(databaseName)
+            .GetCollection<BsonDocument>(collectionName);
+    }
+
+    public List<BsonDocument> Read(BsonDocument filter)
+    {
+        Console.WriteLine("Starting benchmark.");
+        var benchmark = new Benchmark<List<BsonDocument>>(
+            () => _collection.Find(filter).ToList(),
+            filter.ToString());
+        List<BsonDocument> result = benchmark.Run(res => "READ - Count: " + res.Count+ ", Time: {time}, Filter: {input}");
+        Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
+        Console.WriteLine("Totally " + result.Count + " documents found.");
+        return result;
+    }
+
+    public void Delete(BsonDocument filter)
+    {
+        Console.WriteLine("Starting benchmark.");
+        var benchmark = new Benchmark<DeleteResult>(
+            () => _collection.DeleteMany(filter),
+            filter.ToString());
+        benchmark.Run(res => "DELETE - Count: " + res.DeletedCount + ", Time {time}, Filter: {input}");
+        Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
+
+    }
+
+    public void Update(BsonDocument filter, BsonDocument update)
+    {
+        var benchmark = new Benchmark<UpdateResult>(
+            () => _collection.UpdateMany(filter, update),
+            filter.ToString());
+        benchmark.Run(res => "UPDATE - Count: " + res.ModifiedCount + ", Time {time}, Filter: {input}, Updated: " + update);
+        Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
+
+    }
+
+    public void Insert(int numberOfDocuments, IDocumentGenerator documentGenerator)
+    {
+        Console.WriteLine("Documents Generating...");
+        BsonDocument[] documents = documentGenerator.Generate(numberOfDocuments);
+        Console.WriteLine("Bulk Inserting...");
+        var benchmark = new Benchmark<object>(
+            () =>
+            {
+                _collection.InsertMany(documents);
+                return null;
+            },
+            documentGenerator.GetRawTemplate());
+        benchmark.Run(x => "INSERT - Count: " + numberOfDocuments + ", Time: {time}, Template: {input}");
+        Console.WriteLine("Inserted " + numberOfDocuments + " documents.");
+        Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,152 +2,167 @@
 
 // get input
 
+using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
-using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDBBenchmark;
 using MongoDBBenchmark.Generator;
 using static MongoDBBenchmark.InputManager;
 
-const string exitInput = "exit";
-const string backInput = "back";
-IMongoCollection<BsonDocument> collection;
+class Program {
+    private const string ExitInput = "exit";
+    private const string BackInput = "back";
+    private Client _client = new Client();
 
-Console.WriteLine("Welcome to Mongo DB Benchmark.");
-Console.WriteLine("Write 'exit' to exit.");
-ConnectToDatabase();
-while (true)
-    StartNewOperation();
-
-void ConnectToDatabase()
-{
-    MongoClient client;
-    var connectionString = GetStringInput("Please enter connection string: ");
-    try {
-        client = new MongoClient(connectionString);
-    } catch (Exception e) {
-        Console.WriteLine("Could not connected to the database.");
-        Console.WriteLine(e.Message);
-        ConnectToDatabase();
-        return;
-    }
-    var databaseName = GetStringInput("Please enter database name: ");
-    var collectionName = GetStringInput("Please enter collection name: ");
-    collection = client
-        .GetDatabase(databaseName)
-        .GetCollection<BsonDocument>(collectionName);
-}
-
-void StartNewOperation()
-{
-    Operations operation = GetOperationInput();
-    switch (operation)
+    static void Main(string[] args)
     {
-        case Operations.Insert:
-            InsertOperation(GetDocumentGeneratorInput());
-            break;
-        case Operations.Read:
-            ReadOperation();
-            break;
-        case Operations.Update:
-            UpdateOperation();
-            break;
-        case Operations.Delete:
-            DeleteOperation();
-            break;
-        default:
-            throw new ArgumentOutOfRangeException("invalid operation type: " + operation);
-    }
-}
-
-void ReadOperation()
-{
-    BsonDocument filter = GetFilterInput();
-    Console.WriteLine("Starting benchmark.");
-    var benchmark = new Benchmark<List<BsonDocument>>(
-        () => collection.Find(filter).ToList(),
-        filter.ToString());
-    List<BsonDocument> result = benchmark.Run(res => "READ - Count: " + res.Count+ ", Time: {time}, Filter: {input}");
-    Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
-    Console.WriteLine("Totally " + result.Count + " documents found.");
-    Console.WriteLine("Write 'back' to go back to main menu or 'exit' to exit.");
-    if (result.Count > 0)
-    {
-        Console.WriteLine("Write 'show(n)' to show first n documents.");
-        ShowResults(result);
-    }
-}
-
-void ShowResults(List<BsonDocument> result)
-{
-    while (true)
-    {
-        var input = Console.ReadLine();
-        if (input == exitInput)
-            Environment.Exit(0);
-        if (input == backInput)
-            return;
-        if (input == null)
-            continue;
-        if (input.StartsWith("show(") && input.EndsWith(")"))
+        if (args.Length == 1 && args[0] == "--config")
         {
-            int n;
-            if (!Int32.TryParse(input.Substring(5, input.Length - 6), out n))
+            Console.WriteLine("Config mode on. No input will receive.");
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("./operations.json")
+                .Build();
+            new Program().RunUsingConfig(config);
+            return;
+        }
+        new Program().RunUsingCli();
+    }
+
+    void RunUsingConfig(IConfiguration config)
+    {
+        _client.Connect(config["ConnectionString"],
+            config["DatabaseName"],
+            config["CollectionName"]);
+        BsonSerializer.RegisterSerializer(new ObjectSerializer(type => true));
+        foreach (var query in config.GetSection("Operations").GetChildren())
+        {
+            Console.WriteLine("Operation started: " + query.Key);
+            var operation = OperationsExtensions.FromString(query["OperationType"]);
+            if (operation == null)
             {
-                Console.WriteLine("Error! Invalid number.");
+                Console.WriteLine("Invalid operation type: " + query["OperationType"]);
                 continue;
             }
-            for (int i = 0; i < Math.Min(n, result.Count); i++)
+            switch (operation)
             {
-                Console.WriteLine(i + ":" + result[i]);
+                case Operations.Insert:
+                    _client.Insert(Int32.Parse(query["Amount"]), 
+                        new CustomGenerator(DocumentTemplate.FromDocument(Section2BsonDocument(query.GetSection("Template")))));
+                    break;
+                case Operations.Read:
+                    _client.Read(Section2BsonDocument(query.GetSection("Filter")));
+                    break;
+                case Operations.Update:
+                    _client.Update(Section2BsonDocument(query.GetSection("Filter")), 
+                        Section2BsonDocument(query.GetSection("Update")));
+                    break;
+                case Operations.Delete:
+                    _client.Delete(Section2BsonDocument(query.GetSection("Filter")));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("invalid operation type: " + operation);
             }
         }
     }
-}
-
-void DeleteOperation()
-{
-    BsonDocument filter = GetFilterInput();
-    Console.WriteLine("Starting benchmark.");
-    var benchmark = new Benchmark<DeleteResult>(
-        () => collection.DeleteMany(filter),
-        filter.ToString());
-    benchmark.Run(res => "DELETE - Count: " + res.DeletedCount + ", Time {time}, Filter: {input}");
-    Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
-}
-
-void UpdateOperation()
-{
-    BsonDocument filter = GetFilterInput();
-    BsonDocument update = GetDocumentInput("Please enter update document: ");
-    var benchmark = new Benchmark<UpdateResult>(
-        () => collection.UpdateMany(filter, update),
-        filter.ToString());
-    benchmark.Run(res => "UPDATE - Count: " + res.ModifiedCount + ", Time {time}, Filter: {input}, Updated: " + update);
-    Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
-}
-
-void InsertOperation(IDocumentGenerator documentGenerator)
-{
-    int numOfDocuments = GetIntInput("Please enter number of documents to generate: ");
-    Console.WriteLine("Starting benchmark.");
-    Console.WriteLine("Documents Generating...");
-    BsonDocument[] documents = documentGenerator.Generate(numOfDocuments);
-    Console.WriteLine("Bulk Inserting...");
-    var benchmark = new Benchmark<object>(
-        () =>
+    
+    BsonDocument Section2BsonDocument(IConfigurationSection section)
+    {
+        BsonDocument bsonDocument = new BsonDocument();
+        foreach (var child in section.GetChildren())
         {
-            collection.InsertMany(documents);
-            return null;
-        },
-        documentGenerator.GetRawTemplate());
-    benchmark.Run(x => "INSERT - Count: " + numOfDocuments + ", Time: {time}, Template: {input}");
-    Console.WriteLine("Benchmark finished. Time elapsed: " + benchmark.GetElapsedTime());
-    Console.WriteLine("Write 'back' to go back to main menu or 'exit' to exit.");
-    Console.WriteLine("Write anything else to insert again.");
-    var input = Console.ReadLine();
-    if (input == exitInput)
-        Environment.Exit(0);
-    if (input == backInput)
-        return;
-    InsertOperation(documentGenerator);
+             if (child.Value != null) bsonDocument.Add(new BsonElement(child.Key, child.Value));
+             else bsonDocument.Add(new BsonElement(child.Key, Section2BsonDocument(child)));
+        }
+
+        return bsonDocument;
+    }
+
+    void RunUsingCli()
+    {
+        Console.WriteLine("Welcome to Mongo DB Benchmark.");
+        Console.WriteLine("Write 'exit' to exit.");
+        _client.Connect(GetStringInput("Please enter connection string: "),
+            GetStringInput("Please enter database name: "),
+            GetStringInput("Please enter collection name: "));
+        while (true)
+            StartNewOperation();
+    }
+
+    void StartNewOperation()
+    {
+        Operations operation = GetOperationInput();
+        switch (operation)
+        {
+            case Operations.Insert:
+                InsertOperation(GetDocumentGeneratorInput());
+                break;
+            case Operations.Read:
+                ReadOperation();
+                break;
+            case Operations.Update:
+                _client.Update(GetFilterInput(), GetDocumentInput("Please enter update document: "));
+                break;
+            case Operations.Delete:
+                _client.Delete(GetFilterInput());
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("invalid operation type: " + operation);
+        }
+    }
+
+    void ReadOperation()
+    {
+        BsonDocument filter = GetFilterInput();
+        List<BsonDocument> result = _client.Read(filter);
+        Console.WriteLine("Write 'back' to go back to main menu or 'exit' to exit.");
+        if (result.Count > 0)
+        {
+            Console.WriteLine("Write 'show(n)' to show first n documents.");
+            ShowResults(result);
+        }
+    }
+
+    void InsertOperation(IDocumentGenerator documentGenerator)
+    {
+        int numOfDocuments = GetIntInput("Please enter number of documents to generate: ");
+        Console.WriteLine("Starting benchmark.");
+        _client.Insert(numOfDocuments, documentGenerator);
+        Console.WriteLine("Write 'back' to go back to main menu or 'exit' to exit.");
+        Console.WriteLine("Write anything else to insert again.");
+        var input = Console.ReadLine();
+        if (input == ExitInput)
+            Environment.Exit(0);
+        if (input == BackInput)
+            return;
+        InsertOperation(documentGenerator);
+    }
+
+    void ShowResults(List<BsonDocument> result)
+    {
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == ExitInput)
+                Environment.Exit(0);
+            if (input == BackInput)
+                return;
+            if (input == null)
+                continue;
+            if (input.StartsWith("show(") && input.EndsWith(")"))
+            {
+                int n;
+                if (!Int32.TryParse(input.Substring(5, input.Length - 6), out n))
+                {
+                    Console.WriteLine("Error! Invalid number.");
+                    continue;
+                }
+                for (int i = 0; i < Math.Min(n, result.Count); i++)
+                {
+                    Console.WriteLine(i + ":" + result[i]);
+                }
+            }
+        }
+    }
 }
